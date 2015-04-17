d18fba277b0508da559911ac03f8a4e794f5501f
h0wXD <h0wXD@users.noreply.github.com>
2015-04-17 21:03:44 +0200

    Added lest for writing tests.

--- Test/h0wXD.Test/Main.cpp
+++ Test/h0wXD.Test/Main.cpp
@@ -1,11 +1,46 @@
 
 #include "h0wXD/Types.hpp"
+#include <lest.hpp>
+
+#define CASE_E(_NAME) \
+    _NAME, [](env &)
+
+const lest::test no_using_namespace_lest[] =
+{
+    CASE("Namespace lest is specified correctly in lest.hpp [compile-only]")
+    {
+        EXPECT(true);
+        EXPECT_NOT(false);
+        EXPECT_NO_THROW(true);
+        EXPECT_THROWS(true);
+        EXPECT_THROWS_AS(true, std::exception);
+    },
+};
+
+const lest::test BooleanTest[] =
+{
+    CASE("Just testing lest...")
+    {
+        EXPECT(false == false);
+        EXPECT(true == true);
+    },
+};
+
+const lest::test StringTest[] =
+{
+    CASE("Just testing lest...")
+    {
+        EXPECT("123" == std::to_string(123));
+        EXPECT("124" != std::to_string(123));
+    },
+};
 
 int main()
 {
-	using namespace h0wXD;
+	auto iResult = 0;
 
-	int32 bla;
+	iResult |= lest::run(BooleanTest);
+	iResult |= lest::run(StringTest);
 
-	return 0;
+	return iResult;
 }
